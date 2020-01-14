41d790df9be978ada341136889f18dcb00f2baec
James Priest <james.priest@everbridge.com>
2020-01-14 08:51:24 -0800

    refacor: Connect to use before and beforeEach hook

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
+// executed only one time for all test suite
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
+  mongoose.connection
+    .once('open', () => {
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
+// Mocha hook function executed before each test
+beforeEach((done) => {
+  // Direct reference to collection of users in the database
+  // This is an async operation so we call done when complete
+  mongoose.connection.collections.users.drop(() => {
+    // Call done to signal next test
+    done();
+  });
+});
